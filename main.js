c4b1d1d15ce65ae9f16fa835ee930b461e7d0f9d
Mateusz Krause <mateuszkrause.dev@gmail.com>
2023-09-05 19:56:45 +0100

    added console log comment

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const age = 31;
 
 console.log('Siema!');
 console.log(`Mam na imię ${firstName} i mam ${age} lat.`);
+
+const constant = "const";
+let changeable = "let";
+
+console.log(`${constant} to zmienne mające stałe połączenie z jedną wartością.`);
+console.log(`${changeable} to zmienne, które mogą mieć zmienioną wartość, na którą wskazują, ale definiowane są tylko raz.`);
